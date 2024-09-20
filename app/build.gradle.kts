9848cf18ad2ed0aed3316231bf1aaa21e8db74e3
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-20 16:35:45 +0300

    updated: button, textview, edittext

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinbuttontextviewedittext"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinbuttontextviewedittext"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinbuttontextviewedittext/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinbuttontextviewedittext/MainActivity.kt
@@ -2,13 +2,22 @@ package com.ismailmesutmujde.kotlinbuttontextviewedittext
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinbuttontextviewedittext.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
 
-        setContentView(R.layout.activity_main)
+        bindingMain.button.setOnClickListener {
+            val receivedData = bindingMain.editText.text.toString()
+            bindingMain.textView.text = receivedData
+        }
 
     }
 }
